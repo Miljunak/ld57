425c8b9aef512dae1cd91cc491534aca58f45e98
Ech0n <43513980+Ech0n@users.noreply.github.com>
2025-04-05 14:30:23 +0200

    Added input UI

--- submarine/submarine.gd
+++ submarine/submarine.gd
@@ -2,43 +2,85 @@ extends CharacterBody2D
 
 
 @export
-var SPEED = 1000.0
+var SPEED = 70.0
 
 @export
 var WATER_RESISTANCE = 3
 @export
-var BUOYANCY_CHANGE_SPEED = 150
+var BUOYANCY_CHANGE_SPEED = 1
 @export
 var MIN_BUOYANCY = -150
 @export
 var MAX_BUOYANCY = 150
 var buoyancy = 0
+var propulsion = 0.
+@export
+var MAX_PROPULSION = 9
+@export
+var PROPULSION_CHANGE_SPEED = 0.1
+@export
+var INPUT_THRESHOLD = 1
+@export
+var MAX_DESCENT = 150
 
 func _physics_process(delta: float) -> void:
-
-	var propulsion = 0.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		propulsion = direction * SPEED
-	
-	velocity.x += (propulsion - (WATER_RESISTANCE*velocity.x)) * delta
+		propulsion += direction * PROPULSION_CHANGE_SPEED 
+	var clampedProp = clamp_propulsion()
+	set_lever_pos(propulsion)
+	velocity.x += ( (clampedProp* SPEED) - (WATER_RESISTANCE*velocity.x)) * delta 
 	
 	var verticalDirection := Input.get_axis("ui_up", "ui_down")
+	if verticalDirection:
+		buoyancy += verticalDirection  * BUOYANCY_CHANGE_SPEED
+	var clampedBoyancy = clamp_bouyancy(buoyancy)
+	set_boyancy_level(buoyancy)
+	velocity.y = min(max((clampedBoyancy* delta + velocity.y),-MAX_DESCENT),MAX_DESCENT)
 	
-	buoyancy += verticalDirection * delta * BUOYANCY_CHANGE_SPEED
-	
-	velocity.y = clamp_bouyancy(buoyancy+velocity.y)
-	
-	if direction != 0:
-		$aSprite.flip_h  = direction < 0
+	if clampedProp != 0:
+		$aSprite.flip_h  = clampedProp < 0
 		$aSprite.play("running")
 	else:
 		$aSprite.play("idle")
+	print(buoyancy,", ", clampedBoyancy)
 
 	move_and_slide()
+	
+@onready
+var waterLevel = $Camera2D/Control/tank/waterLevel
+func set_boyancy_level(boya):
+	waterLevel.offset.y = boya * 0.1
+
+func _ready() -> void:
+	waterLevel.play("default")
+
+func clamp_propulsion():
+	var local_prop = propulsion
+	if propulsion > MAX_PROPULSION:
+		local_prop = MAX_PROPULSION
+		propulsion = MAX_PROPULSION
+	if propulsion < -MAX_PROPULSION:
+		propulsion = -MAX_PROPULSION
+		local_prop = -MAX_PROPULSION
+	if propulsion < INPUT_THRESHOLD and propulsion > -INPUT_THRESHOLD:
+		local_prop = 0
+	return local_prop
 
 func clamp_bouyancy(_buoyancy: float) -> float:
 	#clamp low bouyances so that u can float in place
 	if _buoyancy < 45 and _buoyancy > -45:
 		return 0
-	return max(min(_buoyancy, MAX_BUOYANCY),MIN_BUOYANCY)
+	buoyancy = max(min(_buoyancy, MAX_BUOYANCY),MIN_BUOYANCY)
+	return buoyancy
+
+@onready
+var lever = $Camera2D/Control/lever
+#func _process(delta: float) -> void:
+	#lever.offset.x = propulsion * 10
+
+func set_lever_pos(prop):
+	lever.offset.x = prop * 10
+	
+func control_throtel():
+	pass
