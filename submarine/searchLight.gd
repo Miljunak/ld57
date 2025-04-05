a63cefa19c265746ad952f1cde468716861af854
Ech0n <43513980+Ech0n@users.noreply.github.com>
2025-04-05 20:53:41 +0200

    Added directionl light steering

---
+++ submarine/searchLight.gd
@@ -0,0 +1,20 @@
+extends PointLight2D
+
+@export
+var rotation_speed = 5
+
+
+@onready
+var base_scale = scale.x
+
+@onready
+var player = get_node("../")
+
+func _process(delta: float) -> void:
+	if player.velocity.x>0:
+		scale.x = base_scale
+	elif  player.velocity.x<0:
+		scale.x = base_scale * -1
+	var lightSteer = Input.get_axis("searchLightDown","searchLightUp")
+	if lightSteer != 0:
+		rotation += lightSteer * rotation_speed * delta
